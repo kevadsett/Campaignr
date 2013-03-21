fc3a63d98e15691ee68c5c6af8925a312e449b31
Fabien O'Carroll <fabienallouis@gmail.com>
2013-03-21 14:41:09 +0000

    Planet And Territory Update

--- app/public/js/collections/collections.js
+++ app/public/js/collections/collections.js
@@ -1,6 +1,19 @@
 var Planet = Backbone.Collection.extend({
     model: Territory,
-    initialize:function() {
-    
+    initialize: function() {
+        this.attributes = {};
+        console.log('New Planet Collection Created');
+        this.on('add', console.log('Territory Pushed'));
+        this.on('change', this.update, this);
     },
+    attribute: function(attr, val){
+        if(val){
+            this.atrributes[attr] = val;
+            return true;
+        }
+        return this.attributes[attr] ;
+    },
+    update: function() {
+        this.sync();
+    }
 });
--- app/public/js/models/models.js
+++ app/public/js/models/models.js
@@ -1,8 +1,10 @@
 var Territory = Backbone.Model.extend({
-    initialize:function() {
+    initialize: function() {
         console.log('New Territory Model Created');
     },
     defaults: {
-        
+        type: 'Default Type',
+        owner: 'Default Owner',
+        notes: 'blank for now'
     }
 });
