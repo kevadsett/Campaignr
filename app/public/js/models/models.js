1f4b483ec63249326fa4073f594c529722dd28d7
Kev Adsett <kev.adsett@gmail.com>
2013-03-21 14:34:48 +0000

    Added player model & collection, faction model.

--- app/public/js/models/models.js
+++ app/public/js/models/models.js
@@ -1,3 +1,28 @@
+var Player = Backbone.Model.extend({
+    defaults:{
+        name: 'UnsetPlayerName',
+        points: 0,
+        notes: []
+    },
+    intialize:function(){
+        console.log('New Player model created');
+    }
+});
+        
+var Faction = Backbone.Model.extend({
+    defaults:{
+        name: 'UnsetFactionName',
+        points: 0
+    },
+    intialize:function(){
+        this.set({players: new PlayerCollection()});
+    }
+});
+
+var PlayerCollection = Backbone.Model.extend({
+    model: Player
+});
+
 var Territory = Backbone.Model.extend({
     initialize: function() {
         console.log('New Territory Model Created');
