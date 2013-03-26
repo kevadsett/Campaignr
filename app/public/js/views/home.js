3a8ab3e79abc9905d8a967c9bebc30ff15643da1
Fabien O'Carroll <fabienallouis@gmail.com>
2013-03-26 17:57:52 +0000

    Big commit
    Main Details: Now turning JSON into backbone entities xx

--- app/public/js/views/home.js
+++ app/public/js/views/home.js
@@ -1,3 +1,23 @@
 
 $(document).ready(function(){
+    function dataSetup(campaign) {
+        var Planets = _.reduce(campaign.planets, function(planets, planet){
+            var p = new Planet(planet.territories);
+            p.collectionAttr("name", planet.name);
+            planets.push(p);
+            return planets;
+        }, []);
+        var campaignModel = new Campaign({
+            name: campaign.name,
+            owner: campaign.owner,
+            players:new Players(campaign.players),
+            battles: new Battles(campaign.battles),
+            factions: new Factions(campaign.factions),
+            planets: Planets
+        });
+        console.log(campaignModel);
+    };
+    for(i=0;i < campaigns.length; i++){
+        dataSetup(campaigns[i].campaign);
+    }
 })
