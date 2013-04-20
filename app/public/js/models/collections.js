317ca5a4c0e8845554427c51ba32a897da0c2793
Fabien <fabienallouis@gmail.com>
2013-04-20 13:26:13 +0100

    cleaned up merge conflicts

--- app/public/js/models/collections.js
+++ app/public/js/models/collections.js
@@ -1,6 +1,5 @@
 var CampaignCollection = Backbone.Collection.extend({
     model: Campaign,
-<<<<<<< HEAD:app/public/js/collections/collections.js
     url: '/db',
     initialize: function() {
         this.on('change', this.render, this);
@@ -8,46 +7,31 @@ var CampaignCollection = Backbone.Collection.extend({
     render: function(){
         console.log(this);
     }
-=======
-    url: '/db'
->>>>>>> CampaignBuild:app/public/js/models/collections.js
 })
 var Planet = Backbone.Collection.extend({
     model: Territory,
     initialize: function() {
-<<<<<<< HEAD:app/public/js/collections/collections.js
         //console.log('New Planet Collection Created');
-=======
->>>>>>> CampaignBuild:app/public/js/models/collections.js
     }
 });
 
 var Players = Backbone.Collection.extend({
     model: Player,
     initialize: function() {
-<<<<<<< HEAD:app/public/js/collections/collections.js
         //console.log('players');
-=======
->>>>>>> CampaignBuild:app/public/js/models/collections.js
     }
 });
 
 var Battles = Backbone.Collection.extend({
     model: Battle,
     initialize: function() {
-<<<<<<< HEAD:app/public/js/collections/collections.js
         //console.log('battles');
-=======
->>>>>>> CampaignBuild:app/public/js/models/collections.js
     }
 });
 
 var Factions = Backbone.Collection.extend({
     model: Faction,
     initialize: function() {
-<<<<<<< HEAD:app/public/js/collections/collections.js
         //console.log('factions');
-=======
->>>>>>> CampaignBuild:app/public/js/models/collections.js
     }
 })
