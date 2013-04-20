df190c4a7e63f701c1a25c396e829fecd6bd424a
Kev Adsett <kev.adsett@gmail.com>
2013-04-20 18:03:30 +0100

    Added planet number to ID in planet partial

--- app/public/js/views/partials/partials.js
+++ app/public/js/views/partials/partials.js
@@ -3,9 +3,14 @@
 Handlebars.partials['newPlanetPartial'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [2,'>= 1.0.0-rc.3'];
 helpers = helpers || Handlebars.helpers; data = data || {};
-  
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
 
 
-  return "<div class=\"newPlanet\">\n    <input name=\"planetName\" placeholder=\"Planet name\" class=\"planetNameTxt\"></input>\n    <label name=\"territoryCount\">Number of territories:</label>\n    <input type=\"number\" name=\"territories\"></input>\n</div>";
+  buffer += "<div class=\"newPlanet";
+  if (stack1 = helpers.planetNumber) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = depth0.planetNumber; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "\">\r\n    <input name=\"planetName\" placeholder=\"Planet name\" class=\"planetNameTxt\"></input>\r\n    <label name=\"territoryCount\">Number of territories:</label>\r\n    <input type=\"number\" name=\"territories\"></input>\r\n</div>";
+  return buffer;
   });
 })();
