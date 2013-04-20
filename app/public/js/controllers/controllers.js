06f099953d30b9ae6f9bbd2af35fb58035d29522
Kev Adsett <kev.adsett@gmail.com>
2013-04-20 13:56:00 +0100

    Moved home controller code to controllers.js

--- app/public/js/controllers/controllers.js
+++ app/public/js/controllers/controllers.js
@@ -37,6 +37,24 @@ var Content = Backbone.View.extend({
 
 var HomeController = Content.extend({
     setup: function () {
+        app.on('appRendered', function() {
+            var numberPlanetsCreated=0;
+            $('#campaignNameTxt').keyup(function(){
+                if(this.value !== ""){
+                    $('.planetCreator').removeClass('hidden');
+                    $('#createCampaignBtn').removeClass('hidden');
+                }else{
+                    $('.planetCreator').addClass('hidden');
+                    $('#createCampaignBtn').addClass('hidden');
+                }
+            });
+            
+            $('#newPlanetButton a').click(function(e){
+                e.preventDefault();
+                numberPlanetsCreated++;
+                $('#planetList').append('<div class="newPlanet" id="newPlanet' + numberPlanetsCreated + '"><input name="planetName" placeholder="Planet name" class="planetNameTxt"></input><label name="territoryCount">Number of territories:</label><input type="number" name="territories"></input></div>');
+            });
+        });
         console.log('homeController');
     }
 });
